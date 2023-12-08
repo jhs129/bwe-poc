3dae11fe57eb9c6d1bb25fad5eb14637ee5b1b89
jhs129 <john.h.schneider@gmail.com>
2023-12-08 00:00:47 -0500

    misc

--- components/ui/card-section.jsx
+++ components/ui/card-section.jsx
@@ -1,8 +1,8 @@
 function CardSection(props) {
   return (
-    <section className="container flex flex-col px-6">
-      <div className="w-full lg:w-2/3 xl:w-1/2"><h2>{props.headline}</h2></div>
-      <div className="flex flex-col md:flex-row">
+    <section className="container flex flex-col">
+      <div className="w-full lg:w-2/3 xl:w-1/2"><h2 className="px-2">{props.headline}</h2></div>
+      <div className="flex flex-col md:flex-row lg:space-x-8">
       {props.children}
       </div>
     </section>
--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
   let linkUrl = props?.linkUrl || "#";
 
   return (
-    <div className="border-black border-t-2 border-b-0 w-[90%] lg:w-72 xl:w-80 mx-6">
+    <div className="border-black border-t-2 border-b-0 lg:w-72 xl:w-80 mx-2 mt-4">
       <div className="mt-4">
         <h4>{title}</h4>
         <div className="body-large mt-4">{body}</div>
