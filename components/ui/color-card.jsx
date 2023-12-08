5eb075aa32c6689a380c060e58b9dd79ec8e6fea
jhs129 <john.h.schneider@gmail.com>
2023-12-08 15:28:26 -0500

    cleaned up unused code

--- components/ui/color-card.jsx
+++ components/ui/color-card.jsx
@@ -2,17 +2,12 @@ function ColorCard(props) {
     let { title, body, color } = props;
 
     // Extracting the unique r, g, b, a values from the color variable
-    let colorClass = 'bg-primaryAccent';
     let r, g, b, a = [0,73,118,1];
 
     const colorPattern = /\d+/g;
     if (color && color.match(colorPattern)) {
         [r, g, b, a] = color.match(colorPattern);
-        colorClass = `bg-[rgba(${r},${g},${b},${a})]`;
-        console.log(colorClass);
     }
-
-    let hexColor = rgbaToHex(r, g, b, a);
 
     const colorBarStyle = {
         backgroundColor: `rgba(${r},${g},${b},${a})`
@@ -28,12 +23,5 @@ function ColorCard(props) {
     );
 };
 
-function rgbaToHex(r, g, b, a) {
-    const hexR = r.toString(16).padStart(2, '0');
-    const hexG = g.toString(16).padStart(2, '0');
-    const hexB = b.toString(16).padStart(2, '0');
-    const hexA = Math.round(a * 255).toString(16).padStart(2, '0');
-    return `#${hexR}${hexG}${hexB}${hexA}`;
-}
 
 export default ColorCard;
