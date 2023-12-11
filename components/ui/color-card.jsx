4b21c36e8ed0f5b8f3d3753f6377754688228dd1
jhs129 <john.h.schneider@gmail.com>
2023-12-11 11:17:57 -0500

    updated color card

--- components/ui/color-card.jsx
+++ components/ui/color-card.jsx
@@ -17,10 +17,14 @@ function ColorCard(props) {
 
     return (
         <div className="flex flex-col relative">
-            <div id="color-bar" className={`justify-center text-center items-center ${txtColor}`} style={colorBarStyle}>
+            <div id="color-bar-desktop" className={`hidden md:flex justify-center text-center items-center ${txtColor}`} style={colorBarStyle}>
                 <h5>{title}</h5>
             </div>
-            <p className="justify-center self-center body-large p-6" dangerouslySetInnerHTML={{ __html: body }}  />
+            <div id="color-bar-mobile" className="flex flex-row md:hidden justify-between items-center pl-6">
+                <div><h5>{title}</h5></div>
+                <div className={`h-2 w-10 ml-2  ${txtColor}`} style={colorBarStyle}></div>
+            </div>
+            <p className="justify-center self-center text-[10px] font-light leading-4 md:text-base md:leading-6 p-6" dangerouslySetInnerHTML={{ __html: body }}  />
         </div>
     );
 };
